234273e52d0a70851480e833f2060fccdf5b465f
desobei <abylaikhan.zhanmolda@gmail.com>
2026-02-05 18:21:16 +0500

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by abylaikhan on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
